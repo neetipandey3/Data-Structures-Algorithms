b03051810947f44c28be30b04f576a4b6d9df505
Neeti <neetipandey3@gmail.com>
2018-08-30 19:28:32 -0700

    sort, search

--- Trees.py
+++ Trees.py
@@ -1,39 +1,70 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+     def __init__(self, x):
+         self.val = x
+         self.left = None
+         self.right = None
+
+
 
 class Trees:
+
+    def build(self, root, node):
+        if root is None:
+            root = node
+        else:
+            if root.val > node.val:
+                if root.left is None:
+                    root.left =  node
+                else:
+                    self.build(root.left, node)
+            else:
+                if root.right is None:
+                    root.right = node
+                else:
+                    self.build(root.right, node)
+
+
     def isValidBST(self, root):
         """
         :type root: TreeNode
         :rtype: bool
+
+        Example 1:
+
+        Input:
+            2
+           / \
+          1   3
+        Output: true
+        Example 2:
+
+            5
+           / \
+          1   4
+             / \
+            3   6
+        Output: false
+        Explanation: The input is: [5,1,4,null,null,3,6]. The root node's value
+                     is 5 but its right child's value is 4.
         """
 
-        def dfs(node):
-            if node is not None:
-                if node.left is not None and node.right is not None and node.left.val < node.right.val:
-                    dfs(node.left)
+        def inorder_trav(node):
+            if node is None:
+                return []
+            return inorder_trav(node.left) + [node.val] + inorder_trav(node.right)
 
-                    def(node.
-
-                        right)
-                    else:
-                    return False
-
-            else:
-                return True
-
-        dfs(root)
-
-
+        inorder_list = inorder_trav(root)
+        return inorder_list == sorted(list(set(inorder_list)))
 
 
 def main():
+    root = TreeNode(2)
     t = Trees()
-    
+    t.build(root, TreeNode(1))
+    t.build(root, TreeNode(3))
+    print(t.isValidBST(root))
+
 
 
 if __name__ == "__main__":
---
+++ sort-search.py
@@ -0,0 +1,26 @@
+class SortAndSearch:
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+
+        Example:
+
+        Input:
+        nums1 = [1,2,3,0,0,0], m = 3
+        nums2 = [2,5,6],       n = 3
+
+        Output: [1,2,2,3,5,6]
+        """
+        while m > 0 and n > 0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+        if m == 0 and n > 0:
+            nums1[:n] = nums2[:n]
