a74c298639fa26b22888e04d46f88edbe8c3a0fd
Neeti <neetipandey3@gmail.com>
2018-09-07 17:16:24 -0700

    array, inplace operations

--- Arrays.py
+++ Arrays.py
@@ -45,6 +45,61 @@ class Array:
         #nums = np.array(nums)
         #return True if np.unique(nums).size < nums.size else False
 
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+
+
+        Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+
+        The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+
+        You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+        Example 1:
+
+        Input: [1,2,3]
+        Output: [1,2,4]
+        Explanation: The array represents the integer 123.
+        Example 2:
+
+        Input: [4,3,2,1]
+        Output: [4,3,2,2]
+        Explanation: The array represents the integer 4321.
+        """
+
+        for i in range(len(digits)):
+            if digits[len(digits) - 1 - i] + 1 > 9:
+                if len(digits) - 1 - i == 0:
+                    return [1] + [0] + digits[1:]
+                digits[len(digits) - 1 - i] = 0
+                self.plusOne(digits[:len(digits) - i])
+            else:
+                digits[len(digits) - 1 - i] += 1
+                return digits
+
+        def moveZeroes(self, nums):
+            """
+            :type nums: List[int]
+            :rtype: void Do not return anything, modify nums in-place instead.
+
+            
+            Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+            Example:
+
+            Input: [0,1,0,3,12]
+            Output: [1,3,12,0,0]
+            Note:
+
+            You must do this in-place without making a copy of the array.
+            Minimize the total number of operations.
+
+            """
+            for _ in range(nums.count(0)):
+                nums.remove(0)
+                nums.append(0)
 
 
 def main():
