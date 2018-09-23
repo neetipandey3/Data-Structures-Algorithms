d3c1ecc543ad32372b25f6e834a74e7392a5d356
Neeti <neetipandey3@gmail.com>
2018-09-23 12:53:13 -0700

    constant space matrix set zeroes

--- Arrays.py
+++ Arrays.py
@@ -173,57 +173,139 @@ class Array:
             """
             matrix[::] = zip(*matrix[::-1])
 
-        def three_sum(self, nums):
-            """
-            :type nums: List[int]
-            :rtype: List[List[int]]
+    def three_sum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
 
 
-            Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+        Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
 
-            Note:
+        Note:
 
-            The solution set must not contain duplicate triplets.
+        The solution set must not contain duplicate triplets.
 
-            Example:
+        Example:
 
-            Given array nums = [-1, 0, 1, 2, -1, -4],
+        Given array nums = [-1, 0, 1, 2, -1, -4],
 
-            A solution set is:
-            [
-              [-1, 0, 1],
-              [-1, -1, 2]
-            ]
+        A solution set is:
+        [
+          [-1, 0, 1],
+          [-1, -1, 2]
+        ]
 
 
 
-            """
-            nums.sort()
-            result = []
-            n = len(nums)
-            if not nums or len(nums) < 3:
-                return []
+        """
+        nums.sort()
+        result = []
+        n = len(nums)
+        if not nums or len(nums) < 3:
+            return []
 
-            for i in range(n - 2):
-                if i > 0 and nums[i] == nums[i - 1]:
-                    continue
-                l, r = i + 1, n - 1
-                while l < r:
-                    sum = nums[i] + nums[l] + nums[r]
-                    if sum == 0:
-                        result.append([nums[i], nums[l], nums[r]])
-                        while l < r and nums[l] == nums[l + 1]:
-                            l += 1
-                        while l < r and nums[r] == nums[r - 1]:
-                            r -= 1
+        for i in range(n - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            l, r = i + 1, n - 1
+            while l < r:
+                sum = nums[i] + nums[l] + nums[r]
+                if sum == 0:
+                    result.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l + 1]:
                         l += 1
+                    while l < r and nums[r] == nums[r - 1]:
                         r -= 1
-                    elif sum > 0:
-                        r -= 1
-                    else:
-                        l += 1
+                    l += 1
+                    r -= 1
+                elif sum > 0:
+                    r -= 1
+                else:
+                    l += 1
 
-            return result
+        return result
+
+    def set_zeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+
+        Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in-place.
+
+        Example 1:
+
+        Input:
+        [
+          [1,1,1],
+          [1,0,1],
+          [1,1,1]
+        ]
+        Output:
+        [
+          [1,0,1],
+          [0,0,0],
+          [1,0,1]
+        ]
+        Example 2:
+
+        Input:
+        [
+          [0,1,2,0],
+          [3,4,5,2],
+          [1,3,1,5]
+        ]
+        Output:
+        [
+          [0,0,0,0],
+          [0,4,5,0],
+          [0,3,1,0]
+        ]
+        Follow up:
+
+        A straight forward solution using O(mn) space is probably a bad idea.
+        A simple improvement uses O(m + n) space, but still not the best solution.
+        Could you devise a constant space solution?
+
+
+
+        """
+
+
+
+        #Constnat space solution
+        if not matrix:
+            return []
+
+        is_row_zero = False
+        is_col_zero = False
+        for i in range(len(matrix)):
+            if matrix[i][0] == 0:
+                is_col_zero = True
+                break
+        for i in range(len(matrix[0])):
+            if matrix[0][i] == 0:
+                is_row_zero = True
+                break
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[0])):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        for row in range(1, len(matrix)):
+            if matrix[row][0] == 0:
+                for i in range(1, len(matrix[0])):
+                    matrix[row][i] = 0
+        for col in range(1, len(matrix[0])):
+            if matrix[0][col] == 0:
+                for i in range(1, len(matrix)):
+                    matrix[i][col] = 0
+
+        if is_row_zero:
+            for i in range(len(matrix[0])):
+                matrix[0][i] = 0
+        if is_col_zero:
+            for i in range(len(matrix)):
+                matrix[i][0] = 0
 
 
 def main():
@@ -237,6 +319,12 @@ def main():
     nums = [1, 2, 3, 4, 5, 6, 7]
     print(a.rotate2(nums, 3))
     print(a.three_sum([-1, 0, -1, 1, 2]))
+    print(a.set_zeroes([
+  [0,1,2,0],
+  [3,4,5,2],
+  [1,3,1,5]
+]))
+
 if __name__ == "__main__":
     main()
 
