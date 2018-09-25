b791431e1d17098b791f889723b2d784a1d23a12
Neeti <neetipandey3@gmail.com>
2018-09-25 11:37:13 -0700

    trees, list

--- binary-tree.py
+++ binary-tree.py
@@ -43,6 +43,41 @@ class Tree:
                 node = node.right
         return result
 
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+
+        '''
+        Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+        
+        For example:
+        Given binary tree [3,9,20,null,null,15,7],
+        
+            3
+           / \
+          9  20
+            /  \
+           15   7
+        return its zigzag level order traversal as:
+        
+        [
+          [3],
+          [20,9],
+          [15,7]
+        ]'''
+        if not root:
+            return []
+        lvl, result = [], []
+        lvl.append(root)
+        zig_zag = 1
+        while lvl:
+            result.append([node.val for node in lvl][::zig_zag])
+            zig_zag *= -1
+            lvl = [child for node in lvl for child in (node.left, node.right) if child]
+        return result
+
 def main():
     root = TreeNode(1)
     t = Tree()
--- linked-list.py
+++ linked-list.py
@@ -103,6 +103,38 @@ class LinkedList:
                 return True
         return False
 
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+
+        '''
+        You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+        
+        You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+        
+        Example:
+        
+        Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+        Output: 7 -> 0 -> 8
+        Explanation: 342 + 465 = 807.
+        '''
+        dummy = cur = ListNode(0)
+        carry = 0
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            cur.next = ListNode(carry % 10)
+            cur = cur.next
+            carry //= 10
+        return dummy.next
+
 
 def main():
 
