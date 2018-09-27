a663351dda366c1bf80e8491eb848e41a8dc07f8
Neeti <neetipandey3@gmail.com>
2018-09-26 21:17:47 -0700

    3 colour sort

--- sort-search.py
+++ sort-search.py
@@ -25,5 +25,51 @@ class SortAndSearch:
         if m == 0 and n > 0:
             nums1[:n] = nums2[:n]
 
+    def top_k_frequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        '''Given a non-empty array of integers, return the k most frequent elements.
+        
+        Example 1:
+        
+        Input: nums = [1,1,1,2,2,3], k = 2
+        Output: [1,2]
+        Example 2:
+        
+        Input: nums = [1], k = 1
+        Output: [1]
+        Note:
+        
+        You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+        Your algorithm's time complexity must be better than O(n log n), where n is the array's size.'''
 
+        from collections import defaultdict
+        d, freq = defaultdict(int), defaultdict(list)
+        for n in nums:
+            d[n] += 1
+        for num, v in d.items():
+            freq[v].append(num)
+
+        result = []
+        for frq in range(len(nums), -1, -1):
+            if frq in freq:
+                result += freq[frq]
+            if len(result) == k:
+                break
+
+        return result
+
+
+def main():
+    s = SortAndSearch()
+    print(s.top_k_frequent([1, 1, 1, 2, 3, 4, 4, 4, 4 , 2]))
+
+
+
+
+if __name__ == "__main__":
+    main()
 
