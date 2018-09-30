5094c773b02bcaccf7bcd98a357ea9d51a813972
Neeti <neetipandey3@gmail.com>
2018-09-29 22:46:40 -0700

    bitwise division integer

--- math.py
+++ math.py
@@ -40,6 +40,60 @@ class Math:
             result += (26 ** (i)) * (ord(c) - ord('A') + 1)
         return result
 
+    def divide_integers_bitwise(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+
+        '''
+        Given two integers dividend and divisor, divide two integers without using multiplication, division and mod operator.
+
+        Return the quotient after dividing dividend by divisor.
+        
+        The integer division should truncate toward zero.
+        
+        Example 1:
+        
+        Input: dividend = 10, divisor = 3
+        Output: 3
+        Example 2:
+        
+        Input: dividend = 7, divisor = -3
+        Output: -2
+        Note:
+        
+        Both dividend and divisor will be 32-bit signed integers.
+        The divisor will never be 0.
+        Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 231 − 1 when the division result overflows.'''
+
+        negative = False
+        if (dividend < 0 and divisor > 0) or (dividend > 0 and divisor < 0):
+            negative = True
+        if dividend == -2 ** 31 and divisor == -1:
+            return 2 ** 31 - 1
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+
+        result = 0
+        while dividend >= divisor:
+            quotient = 1
+            div = divisor
+            while div << 1 <= dividend:
+                div <<= 1
+                quotient <<= 1
+            dividend -= div
+            result += quotient
+
+        if negative:
+            return -result
+        else:
+            return result
+
+
+
+
 def main():
     m = Math()
     m.title_to_number("ZY")
